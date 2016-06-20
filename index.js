e9f47eb9deb98d2ca7f0367e7d7b1529588c2a21
Eugene Datsky <eugene@datsky.ru>
2016-06-20 11:10:02 +0300

    .editorconfig and reformat

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-banner', function (opts) {
-    opts = opts || {};
+  opts = opts || {};
 
-    function process(value) {
-        var text = String(value);
-        var comment = text;
+  function process(value) {
+    var text = String(value);
+    var comment = text;
 
-        if (!opts.inline) {
-            comment = ['']
-                .concat(text.split('\n'))
-                .join('\n * ')
-                .concat('\n');
-        } else {
-          comment = [' ', comment, ' '].join('');
-        }
-
-        comment = ['/*!', comment, '*/']
-            .join('');
-
-        return comment;
+    if (!opts.inline) {
+      comment = [''].
+        concat(text.split('\n')).
+        join('\n * ').
+        concat('\n');
+    } else {
+      comment = [' ', comment, ' '].join('');
     }
 
-    return function (css) {
-        if ('banner' in opts) {
-            css.prepend(process(opts.banner));
+    comment = ['/*!', comment, '*/'].join('');
 
-            // New line after banner
-            if (css.nodes[1]) {
-                css.nodes[1].raws.before = '\n';
-            }
-        }
+    return comment;
+  }
 
-        if ('footer' in opts) {
-            var footer = process(opts.footer);
-            css.append(footer);
-            css.nodes[css.nodes.length - 1].raws.before = '\n';
-        }
-    };
+  return function (css) {
+    if ('banner' in opts) {
+      css.prepend(process(opts.banner));
+
+      // New line after banner
+      if (css.nodes[1]) {
+        css.nodes[1].raws.before = '\n';
+      }
+    }
+
+    if ('footer' in opts) {
+      var footer = process(opts.footer);
+      css.append(footer);
+      css.nodes[css.nodes.length - 1].raws.before = '\n';
+    }
+  };
 });
--- test/test.js
+++ test/test.js
@@ -4,63 +4,70 @@ var expect = require('chai').expect;
 var plugin = require('../');
 
 var testProcess = function (input, output, opts) {
-    expect(postcss(plugin(opts)).process(input).css).to.eql(output);
+  expect(postcss(plugin(opts)).process(input).css).to.eql(output);
 };
 
 var input = 'a{ decl: value; }';
 
 var multilineText = ['multi',
-    'line',
-    'comment'
-  ].join('\n');
+  'line',
+  'comment'
+].join('\n');
 
 var multilineResult = '' +
-    '/*!\n' +
-    ' * multi\n' +
-    ' * line\n' +
-    ' * comment\n' +
-    '*/';
+  '/*!\n' +
+  ' * multi\n' +
+  ' * line\n' +
+  ' * comment\n' +
+  '*/';
 
 describe('postcss-banner', function () {
-    describe('banner', function () {
-        it('should add banner', function () {
-            testProcess(input, '/*!\n * LOL\n*/\na{ decl: value; }', {banner: 'LOL'});
-        });
-
-        it('should render the comment inline', function () {
-            testProcess(input, '/*! LOL */\na{ decl: value; }', {banner: 'LOL', inline: true});
-        });
-
-        it('should add multiline banner', function () {
-            testProcess(input, multilineResult + '\na{ decl: value; }', {banner: multilineText});
-        });
+  describe('banner', function () {
+    it('should add banner', function () {
+      testProcess(input, '/*!\n * LOL\n*/\na{ decl: value; }', {banner: 'LOL'});
     });
 
-    describe('footer', function () {
-        it('should add footer', function () {
-            testProcess(input, 'a{ decl: value; }\n/*!\n * LOL\n*/', {footer: 'LOL'});
-        });
-
-        it('should add footer inline', function () {
-            testProcess(input, '/*! LOL */\na{ decl: value; }', {banner: 'LOL', inline: true});
-        });
-
-        it('should add multiline footer', function () {
-            testProcess(input, 'a{ decl: value; }\n' + multilineResult, {footer: multilineText});
-        });
+    it('should render the comment inline', function () {
+      testProcess(input, '/*! LOL */\na{ decl: value; }', {banner: 'LOL', inline: true});
     });
 
-    describe('both', function () {
-        it('should add banner and footer', function () {
-            testProcess(input, '/*!\n * banner\n*/\na{ decl: value; }\n/*!\n * footer\n*/', {footer: 'footer', banner: 'banner'});
-        });
-
-        it('should convert values to string', function () {
-            testProcess(input, '/*! undefined */\na{ decl: value; }\n/*! undefined */', {banner: undefined, footer: undefined, inline: true});
-        });
-
-        it('should ignore not set values', function () {
-            testProcess(input, input, {});
-        });
+    it('should add multiline banner', function () {
+      testProcess(input, multilineResult + '\na{ decl: value; }', {banner: multilineText});
     });
+  });
+
+  describe('footer', function () {
+    it('should add footer', function () {
+      testProcess(input, 'a{ decl: value; }\n/*!\n * LOL\n*/', {footer: 'LOL'});
+    });
+
+    it('should add footer inline', function () {
+      testProcess(input, '/*! LOL */\na{ decl: value; }', {banner: 'LOL', inline: true});
+    });
+
+    it('should add multiline footer', function () {
+      testProcess(input, 'a{ decl: value; }\n' + multilineResult, {footer: multilineText});
+    });
+  });
+
+  describe('both', function () {
+    it('should add banner and footer', function () {
+      testProcess(input, '/*!\n * banner\n*/\na{ decl: value; }\n/*!\n * footer\n*/', {
+        footer: 'footer',
+        banner: 'banner'
+      });
+    });
+
+    it('should convert values to string', function () {
+      testProcess(input, '/*! undefined */\na{ decl: value; }\n/*! undefined */', {
+        banner: undefined,
+        footer: undefined,
+        inline: true
+      });
+    });
+
+    it('should ignore not set values', function () {
+      testProcess(input, input, {});
+    });
+  });
 });
