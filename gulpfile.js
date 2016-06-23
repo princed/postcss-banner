9ac5f99cb99c7a5bb41644470fa1dd941228a0d8
Eugene Datsky <eugene@datsky.ru>
2016-06-23 14:05:05 +0300

    Revert space-before-function-paren settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src(['index.js', 'test/*.js', 'gulpfile.js'])
     .pipe(eslint({
       quiet: true
@@ -11,7 +11,7 @@ gulp.task('lint', function() {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function() {
+gulp.task('test', function () {
   return gulp.src('test/*.js', {read: false})
     .pipe(mocha());
 });
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 
 var plugin = require('../');
 
-var testProcess = function(input, output, opts) {
+var testProcess = function (input, output, opts) {
   expect(postcss(plugin(opts)).process(input).css).to.eql(output);
 };
 
@@ -33,43 +33,43 @@ var multilineBangResult = [
   ' */'
 ].join('\n');
 
-describe('postcss-banner', function() {
-  describe('banner', function() {
-    it('should add banner', function() {
+describe('postcss-banner', function () {
+  describe('banner', function () {
+    it('should add banner', function () {
       testProcess(input, '/*\n * LOL\n */\na{ decl: value; }',
         {banner: 'LOL'});
     });
 
-    it('should render the comment inline', function() {
+    it('should render the comment inline', function () {
       testProcess(input, '/* LOL */\na{ decl: value; }',
         {banner: 'LOL', inline: true});
     });
 
-    it('should add multiline banner', function() {
+    it('should add multiline banner', function () {
       testProcess(input, multilineResult + '\na{ decl: value; }',
         {banner: multilineText});
     });
   });
 
-  describe('footer', function() {
-    it('should add footer', function() {
+  describe('footer', function () {
+    it('should add footer', function () {
       testProcess(input, 'a{ decl: value; }\n/*\n * LOL\n */',
         {footer: 'LOL'});
     });
 
-    it('should add footer inline', function() {
+    it('should add footer inline', function () {
       testProcess(input, '/* LOL */\na{ decl: value; }',
         {banner: 'LOL', inline: true});
     });
 
-    it('should add multiline footer', function() {
+    it('should add multiline footer', function () {
       testProcess(input, 'a{ decl: value; }\n' + multilineResult,
         {footer: multilineText});
     });
   });
 
-  describe('both', function() {
-    it('should add banner and footer', function() {
+  describe('both', function () {
+    it('should add banner and footer', function () {
       testProcess(input, '/*\n * banner\n */\na{ decl: value; }\n/*\n * footer\n */',
         {
           footer: 'footer',
@@ -77,7 +77,7 @@ describe('postcss-banner', function() {
         });
     });
 
-    it('should convert values to string', function() {
+    it('should convert values to string', function () {
       testProcess(input, '/* undefined */\na{ decl: value; }\n/* undefined */',
         {
           banner: undefined,
@@ -86,17 +86,17 @@ describe('postcss-banner', function() {
         });
     });
 
-    it('should add inline bang decorator', function() {
+    it('should add inline bang decorator', function () {
       testProcess(input, '/*! LOL */\na{ decl: value; }',
         {banner: 'LOL', inline: true, important: true});
     });
 
-    it('should add multiline bang decorator', function() {
+    it('should add multiline bang decorator', function () {
       testProcess(input, multilineBangResult + '\na{ decl: value; }',
         {banner: multilineText, important: true});
     });
 
-    it('should ignore not set values', function() {
+    it('should ignore not set values', function () {
       testProcess(input, input, {});
     });
   });
